9af13229233d88a27e57ccddc43d66d20de64c7e
Filip Bernt <filip.bernt@gmail.com>
2025-11-09 17:25:31 +0100

    fixed port

--- server.js
+++ server.js
@@ -293,7 +293,5 @@ app.get("/system-config", (req, res) => {
 	});
 });
 
-const PORT = 3000;
-app.listen(PORT, () =>
-	console.log(`Server started on http://localhost:${PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server started`));
